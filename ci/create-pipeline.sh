f56365ad960b387952472a12770715f83635b306
fox0r47 <extern.christian.lange1@volkswagen.de>
2019-05-29 12:05:11 +0200

    Add create/destroy for all pipelines

--- ci/create-pipeline.sh
+++ ci/create-pipeline.sh
@@ -5,9 +5,6 @@ set -e
 #see https://blog.osones.com/en/multi-git-branches-workflow-with-concourse-ci.html
 #see https://github.com/vito/git-branches-resource
 
-export NEW_VERSIONS=$(cat branches/branches)
-export OLD_VERSIONS=$(cat branches/removed)
-
 FLY_CMD=fly3
 
 $FLY_CMD login -t ci -c $FLY_CONCOURSE_URL -n $FLY_TEAM -u $FLY_USERNAME -p $FLY_PASSWORD
@@ -17,6 +14,31 @@ GIT_FULL_URI=${GIT_URI/https:\/\//https:\/\/$GIT_USERNAME:$GIT_PASSWORD@}
 
 echo git clone $GIT_FULL_URI source-repository
 git clone $GIT_FULL_URI source-repository
+
+if [ "$1" = "create" ]
+then
+	cd source-repository
+	NEW_VERSIONS=$(git branch -a | grep remotes/origin | grep -v -F -- "->")
+	NEW_VERSIONS=${NEW_VERSIONS//remotes\/origin\//}
+	OLD_VERSIONS=
+	cd ..
+fi
+
+if [ "$1" = "destroy" ]
+then
+	OLD_PIPELINES=$($FLY_CMD -t ci pipelines | grep $PROJECTNAME | grep -v $PROJECTNAME-auto-pipeline | sed -e 's/  *.*//g')
+	for pipe_name in $OLD_PIPELINES; do
+		echo "Delete pipeline $pipe_name"
+		$FLY_CMD -t ci destroy-pipeline --pipeline $pipe_name -n || true
+	done
+	exit
+fi
+
+if [ "$1" = "" ]
+then
+	export NEW_VERSIONS=$(cat branches/branches)
+	export OLD_VERSIONS=$(cat branches/removed)
+fi
 
 for version in $NEW_VERSIONS; do
   pipe_name=${version//\//-}
